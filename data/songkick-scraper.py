3068fb69c1a2bf38ae25580fea16d83d8c969832
Zach Maurer <zach.maurer@gmail.com>
2016-05-16 12:32:17 -0700

    created a ratelimit module

--- data/songkick-scraper.py
+++ data/songkick-scraper.py
@@ -7,37 +7,16 @@ import requests
 from pymongo import MongoClient
 
 from key import API_KEY
+from ratelimit import RateLimited
 
-
-#Rate limiting from http://blog.gregburek.com/2011/12/05/Rate-limiting-with-decorators/
-def RateLimited(maxPerSecond):
-    minInterval = 1.0 / float(maxPerSecond)
-    def decorate(func):
-        lastTimeCalled = [0.0]
-        def rateLimitedFunction(*args,**kargs):
-            elapsed = time.clock() - lastTimeCalled[0]
-            leftToWait = minInterval - elapsed
-            if leftToWait>0:
-                time.sleep(leftToWait)
-            ret = func(*args,**kargs)
-            lastTimeCalled[0] = time.clock()
-            return ret
-        return rateLimitedFunction
-    return decorate
-
-@RateLimited(0.1)  # 1 per 10 seconds
+@RateLimited(0.2)  # 1 per 10 seconds
 def requestArtist(artist_name):
-    print(num)
-
-
+    print(artist_name)
 
 def getArtistIDs(artists):
   for a in artists:
-    PrintNumber(a)
-
-
-
-
+    requestArtist(a)
+    
 # Reads in .txt file from command line.
 # Creates array of strings that are used to perform the artist lookup.
 def readArtistsFromFile():
